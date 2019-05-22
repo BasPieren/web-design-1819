795fbb59da7263c74e79c42b5ca7a18527877e99
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-22 12:03:13 +0200

    ♻️ Removed unneeded functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
 import { formSounds } from './modules/sounds.js'
-import { choicesLocalStorage } from './modules/choices.js'
-import { renderChoices } from './modules/render.js'
 
 formSounds()
-choicesLocalStorage()
-renderChoices()
